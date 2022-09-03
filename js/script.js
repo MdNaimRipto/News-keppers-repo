35c7a40bf000bb97486657f88114a2ee1ee00fc1
MdNaimRipto <naimurtsc567@gmail.com>
2022-09-03 12:45:06 +0600

    news section updated

--- js/script.js
+++ js/script.js
@@ -34,25 +34,53 @@ const displayCategoryContent = (allNews) => {
         noNews.classList.add("d-none")
     }
     allNews.forEach(news => {
-        const { thumbnail_url, title, details } = news;
+        const { thumbnail_url, title, details, total_view } = news;
         const newsDiv = document.createElement("div");
         newsDiv.classList.add("col")
         newsDiv.innerHTML = `
         <div class="card mb-3 mt-5">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="${thumbnail_url}" class="img-fluid" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">${title}</h5>
-              <p class="card-text">${details.slice(0, 500)}</p>
-              <div></div>
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${thumbnail_url}" class="img-fluid" alt="...">
+                </div>
+                <div class="col-md-8 mt-3">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${details.slice(0, 400) + '...'}</p>
+                </div >
+                <div class="container text-center mt-2">
+                    <div class="row">
+                        <div class="col">
+                            <div class="d-flex align-items-center">
+                                <div class="category-image me-3">
+                                    <img src="${news.author.img}" class="w-100">
+                                </div>
+                            <div class="author-details">
+                                <h6>${news.author.name}</h6>
+                                <p class="mb-0">${news.author.published_date}</p>
+                            </div>
+                        </div>
+                    </div>
+                <div class="col">
+                    <div class="pt-2">
+                        <i class="fa-regular fa-eye"></i> ${total_view}
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="pt-2">
+                        <p>Rating:${news.rating.number}</p>
+                    </div>
+                </div>
+                    <div class="col">
+                        <button class="full-news-btn pt-2">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </button>
+                    </div>
+                </div>
+
             </div>
-          </div>
         </div>
-      </div>
-        `
+    `
         newsContainer.appendChild(newsDiv)
     })
 }
