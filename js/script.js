859dbd92581d2fe63d6699d8165194a351c4872a
MdNaimRipto <naimurtsc567@gmail.com>
2022-09-03 15:06:20 +0600

    total item count section completed

--- js/script.js
+++ js/script.js
@@ -26,6 +26,13 @@ const loadCategoryContent = (category_id) => {
 const displayCategoryContent = (allNews) => {
     const newsContainer = document.getElementById("news-container");
     newsContainer.innerHTML = ``;
+    const totalItems = document.getElementById("item-number");
+    if (allNews.length === 0) {
+        totalItems.innerText = `0 Items found`
+    }
+    else {
+        totalItems.innerText = `${allNews.length} Items found`
+    }
     const noNews = document.getElementById("no-news");
     if (allNews.length === 0) {
         noNews.classList.remove("d-none")
@@ -40,39 +47,39 @@ const displayCategoryContent = (allNews) => {
         newsDiv.innerHTML = `
         <div class="card mb-3 mt-5">
             <div class="row g-0">
-                <div class="col-md-4 w-25">
-                    <img src="${thumbnail_url}" class="img-fluid" alt="...">
+                <div class="col-md-4 news-thumbnail">
+                    <img src="${thumbnail_url}" class="p-0 img-thumbnail" alt="...">
                 </div>
                 <div class="col-md-8 mt-3">
                     <div class="card-body">
-                        <h5 class="card-title">${title}</h5>
-                        <p class="card-text">${details.slice(0, 400) + '...'}</p>
+                        <h5 class="card-title mb-4">${title}</h5>
+                        <p class="card-text">${details.slice(0, 300) + '...'}</p>
                 </div >
                 <div class="container text-center mt-2">
                     <div class="row">
                         <div class="col">
                             <div class="d-flex align-items-center">
-                                <div class="category-image me-3">
+                                <div class="category-image me-3 author-img">
                                     <img src="${news.author.img}" class="w-100">
                                 </div>
                             <div class="author-details">
-                                <h6>${news.author.name}</h6>
-                                <p class="mb-0">${news.author.published_date}</p>
+                                <h6 class="author-name">${news.author.name}</h6>
+                                <p class="mb-0 publishing-date">${news.author.published_date}</p>
                             </div>
                         </div>
                     </div>
                 <div class="col">
-                    <div class="pt-2">
-                        <i class="fa-regular fa-eye"></i> ${total_view}
+                    <div class="pt-2 text-size">
+                        <i class="fa-regular fa-eye"></i> <span>${total_view}</span>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="pt-2">
+                    <div class="pt-2 text-size">
                         <p>Rating:${news.rating.number}</p>
                     </div>
                 </div>
                     <div class="col">
-                        <button class="full-news-btn pt-2">
+                        <button class="full-news-btn text-size pt-2">
                             <i class="fa-solid fa-arrow-right"></i>
                         </button>
                     </div>
